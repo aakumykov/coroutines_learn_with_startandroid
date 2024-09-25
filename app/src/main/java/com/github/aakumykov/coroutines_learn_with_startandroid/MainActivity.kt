844707938a454e68c2525140c7dcd256b2153e63
Andrey Kumykov <aakumykov@yandex.ru>
2024-09-25 21:04:37 +0300

    Экран с кнопкой.

--- app/src/main/java/com/github/aakumykov/coroutines_learn_with_startandroid/MainActivity.kt
+++ app/src/main/java/com/github/aakumykov/coroutines_learn_with_startandroid/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.github.aakumykov.coroutines_learn_with_startandroid
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.github.aakumykov.coroutines_learn_with_startandroid.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val TAG: String = MainActivity::class.java.simpleName
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding.launchButton1.setOnClickListener { onLaunch1ButtonClicked() }
+    }
+
+    private fun onLaunch1ButtonClicked() {
+        Log.d(TAG, "onLaunch1ButtonClicked() called")
     }
 }
