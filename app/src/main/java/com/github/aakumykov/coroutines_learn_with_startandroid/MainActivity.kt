89b65b5b8da848eb21686f615082c26e2caa2d3d
Andrey Kumykov <aakumykov@yandex.ru>
2024-09-25 21:54:34 +0300

    Ага, запуск suspend-функции через самодельный Scope не требует применения в этой функции увода в фоновый поток, так как корутина выполняется в DefaultDispatcher-worker-ах.

--- app/src/main/java/com/github/aakumykov/coroutines_learn_with_startandroid/MainActivity.kt
+++ app/src/main/java/com/github/aakumykov/coroutines_learn_with_startandroid/MainActivity.kt
@@ -2,11 +2,18 @@ package com.github.aakumykov.coroutines_learn_with_startandroid
 
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.enableEdgeToEdge
+import android.util.TimeUtils
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import com.github.aakumykov.coroutines_learn_with_startandroid.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import java.io.File
+import java.util.concurrent.TimeUnit
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +24,11 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val networkService: NetworkService by lazy { NetworkService(cacheDir) }
+
+    private val coroutineScope: CoroutineScope by lazy { CoroutineScope(Job()) }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -26,7 +38,47 @@ class MainActivity : AppCompatActivity() {
         binding.launchButton1.setOnClickListener { onLaunch1ButtonClicked() }
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        coroutineScope.cancel()
+    }
+
+    private fun log(text: String) {
+        Log.d(TAG, "[${Thread.currentThread().let { "${it.name}{${hashCode()}}" }}] $text")
+    }
+
     private fun onLaunch1ButtonClicked() {
-        Log.d(TAG, "onLaunch1ButtonClicked() called")
+        log("-------------------------------------------")
+        log("Перед запуском suspend-фнукции")
+        coroutineScope.launch {
+            download("файл://путь-к-файлу.txt")
+        }
+        log("После запуска suspend-фнукции")
+    }
+
+    private suspend fun download(url: String): File {
+        log("suspend fun download(), перед запуском suspendCoroutine{}")
+        return suspendCoroutine { continuation ->
+            log("suspend fun download(), внутри suspendCoroutine, перед networkService.download()")
+            networkService.download(url, object: NetworkService.Callback {
+                override fun onSuccess(downloadedFile: File) {
+                    log("suspend fun download(), внутри suspendCoroutine, внутри networkService.download()")
+                    continuation.resume(downloadedFile)
+                }
+            })
+        }
+    }
+
+    class NetworkService(private val cacheDir: File) {
+
+        fun download(url: String, callback: Callback) {
+            TimeUnit.SECONDS.sleep(3)
+            callback.onSuccess(File(cacheDir,"some_file.txt"))
+        }
+
+        interface Callback {
+            fun onSuccess(downloadedFile: File)
+//            fun onError(e: Exception)
+        }
     }
 }
