97df2d8c4383256dc778a53d69127b8da55adc36
Andrey Kumykov <aakumykov@yandex.ru>
2024-10-24 11:38:01 +0300

    Revert "Начал делать в графике, но нужно ли?"
    This reverts commit fabf278cdf2459d5c22b747f8480a0aded7ac31a.

--- app/src/main/java/com/github/aakumykov/coroutines_learn_with_startandroid/MainActivity.kt
+++ app/src/main/java/com/github/aakumykov/coroutines_learn_with_startandroid/MainActivity.kt
@@ -96,43 +96,24 @@ class MainActivity : AppCompatActivity() {
         scope.launch {
 
             log("Перед запуском 1 launch")
-            val job1 = scope.launch (start = startType()) {
+            val job1 = scope.launch (start = CoroutineStart.LAZY) {
                 TimeUnit.SECONDS.sleep(2)
                 log("@@@ работа launch-1")
-            }.let {
-                if (isImmediateStart()) it.join()
-                else it
-            }
+            }//.join()
             log("После запуска 1 launch")
 
 
             log("Перед запуском 2 launch")
-            val job2 = scope.launch (start = startType()) {
+            val job2 = scope.launch (start = CoroutineStart.LAZY) {
                 TimeUnit.SECONDS.sleep(3)
                 log("@@@ работа launch-2")
-            }.also {
-                if (isImmediateStart()) it.join()
-                else it
-            }
+            }//.join()
             log("После запуска 2 launch")
 
-            if (isDeferredRun()) {
-                (job1 as Job).join()
-                job2.join()
-            }
+            job1.join()
+            job2.join()
         }
     }
-
-    private fun startType(): CoroutineStart {
-        return when (binding.startType.checkedRadioButtonId) {
-            R.id.coroutineStartDefault -> CoroutineStart.DEFAULT
-            else -> CoroutineStart.LAZY
-        }
-    }
-
-    private fun isImmediateStart(): Boolean = binding.startType.checkedRadioButtonId == R.id.coroutineStartDefault
-    private fun isDeferredRun(): Boolean = binding.runType.checkedRadioButtonId == R.id.deferredRun
-
     /*
     * Как .join() и параметр 'start' вызова .launch() влияет на работу параллельных корутин:
     * 1) CoroutineStart.DEFAULT + немедленный .join() = последовательная работа,
