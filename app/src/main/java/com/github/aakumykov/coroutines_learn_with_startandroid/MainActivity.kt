540db2d8a142a628ba95a492d884af4fd14f516e
Andrey Kumykov <aakumykov@yandex.ru>
2024-10-26 21:24:00 +0300

    Ошибка в соседней корутине того же Scope.

--- app/src/main/java/com/github/aakumykov/coroutines_learn_with_startandroid/MainActivity.kt
+++ app/src/main/java/com/github/aakumykov/coroutines_learn_with_startandroid/MainActivity.kt
@@ -7,6 +7,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.github.aakumykov.coroutines_learn_with_startandroid.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.coroutines.cancellation.CancellationException
+import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,20 +25,57 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private var _scope: CoroutineScope? = null
+    private val scope: CoroutineScope get() = _scope!!
+
+    private fun prepareScope() {
+        _scope = CoroutineScope(Job())
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _scope?.cancel(CancellationException("onDestroy()"))
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
         binding.launchButton1.setOnClickListener { onLaunch1ButtonClicked() }
     }
 
     private fun onLaunch1ButtonClicked() {
-        Log.d(TAG, "onLaunch1ButtonClicked() called")
+        prepareScope()
+
+        val coroutineExceptionHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
+            log("*** coroutineExceptionHandler ***")
+            Log.e(TAG, throwable.message, throwable)
+        }
+
+        scope.launch (coroutineExceptionHandler) {
+            log("Родительская корутина")
+
+            launch {
+                repeat(5) { i ->
+                    log("дочерняя-корутина-1 ... ${i+1}")
+                    delay(500)
+                }
+            }
+
+            launch {
+                delay(Random.nextLong(100, 1501))
+                log("дочерняя-корутина-2")
+                Integer.parseInt("a")
+            }
+        }
+    }
+
+    private fun log(text: String) {
+        val thread = Thread.currentThread()
+        val threadName = thread.name
+        val threadHashCode = thread.hashCode()
+        Log.d(TAG, "[$threadName {$threadHashCode}] $text")
     }
 }
